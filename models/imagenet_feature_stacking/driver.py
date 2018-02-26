7bc2384a16a6bc67beb16189d8caf236545ea544
Arjun Blum <arjunblum@gmail.com>
2018-02-26 15:48:02 -0500

    Tuned CV term for logistic regression. Added inception to feature generating models.

--- models/imagenet_feature_stacking/driver.py
+++ models/imagenet_feature_stacking/driver.py
@@ -5,11 +5,14 @@ from random import shuffle, randint, seed
 from keras.preprocessing.image import ImageDataGenerator
 from keras.applications.resnet50 import preprocess_input as preprocess_input_resnet
 from keras.applications.xception import preprocess_input as preprocess_input_xception
+from keras.applications.inception_v3 import preprocess_input as preprocess_input_inception
 from keras.models import load_model
 from keras.applications import ResNet50
 from keras.applications import Xception
+from keras.applications import InceptionV3
 from keras.models import Sequential
 from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.metrics import log_loss, accuracy_score
 
 
@@ -21,7 +24,10 @@ def create_l1_models():
     xception = Sequential()
     xception.add(Xception(include_top=False, pooling='avg', weights='imagenet'))
     models.append(xception)
-    preprocessors = [preprocess_input_resnet, preprocess_input_xception]
+    preprocessors = [preprocess_input_resnet, preprocess_input_xception, preprocess_input_inception]
+    inception = Sequential()
+    inception.add(InceptionV3(include_top=False, pooling='avg', weights='imagenet'))
+    models.append(inception)
     return models, preprocessors
 
 
@@ -36,24 +42,28 @@ def generate_features(models, preprocessers):
 
 
 def train_l2(features, labels, val=False):
-    model = LogisticRegression(multi_class='multinomial', solver='lbfgs')
+    # for c in [.01,.02,.03,.04,.05,.06,.07,.08,.1]:
+    model = LogisticRegression(multi_class='multinomial', solver='lbfgs', C=.03)
     model.fit(features['train'],labels['train'])
     if val:
         probs = model.predict_proba(features['validation'])
         preds = model.predict(features['validation'])
+        # print('C = ', c)
         print('Validation LogLoss {}'.format(log_loss(labels['validation'], probs)))
-        print('Validation Accuracy {}'.format(accuracy_score((labels['validation'] * range(120)).sum(axis=1), preds)))
+        print('Validation Accuracy {}'.format(accuracy_score(labels['validation'], preds)))
     return model
 
 def generate_predictions(models, preprocessers, level2):
     all_features = []
     for i in range(len(models)):
         generator = ImageDataGenerator(preprocessing_function=preprocessers[i])
-        pred_generator = generator.flow_from_directory('../../input/train', class_mode=None, shuffle=False,
+        pred_generator = generator.flow_from_directory('../../input/test', class_mode=None, shuffle=False,
                                                        batch_size=64,
                                                        target_size=(224, 224))
         all_features.append(models[i].predict_generator(generator=pred_generator, steps=len(pred_generator), verbose=1))
     X = np.concatenate(all_features, axis=-1)
+    save2 = pd.DataFrame(X)
+    save2.to_csv('savePred.csv',index_label=False)
     y = level2.predict_proba(X)
     ids = [x[:-4] for x in os.listdir("../../input/test/test")]
     labels_df = pd.read_csv('../../input/labels.csv')
@@ -61,15 +71,20 @@ def generate_predictions(models, preprocessers, level2):
     labels_list.sort()
     submission = pd.DataFrame(data=y, columns=labels_list)
     submission.to_csv('temp.csv')
-    submission = pd.read_csv('test_only_y.csv', index_col=0)
+    submission = pd.read_csv('temp.csv', index_col=0)
     submission.insert(0, 'id', ids)
-    submission.to_csv('logreg_imagenet_features.csv', index=False)
+    submission.to_csv('logreg_imagenet_features_v2.csv', index=False)
 
 
 if __name__ == '__main__':
     models, preprocess = create_l1_models()
-    X = generate_features(models,preprocess)
-
+    # X = generate_features(models,preprocess)
+    # labels_df = pd.read_csv('../../input/labels.csv')
+    # labels_list = list(set(labels_df['breed']))
+    # save = pd.DataFrame(X)
+    # save.to_csv('save.csv', index_label=False)
+    X = pd.read_csv('save.csv')
+    X.drop('0',axis=1)
     labels = pd.read_csv('../../input/labels.csv')
     labels['target'] = 1
     labels['rank'] = labels.groupby('breed').rank()['id']
@@ -78,9 +93,12 @@ if __name__ == '__main__':
 
     np.random.seed(seed=420)
     rnd = np.random.random(len(labels))
-    train_idx = rnd < 0.8
-    valid_idx = rnd >= 0.8
+    train_idx = rnd < 2
+    valid_idx = rnd >= 2
     y_train = np.array([y[1:] for y in labels_pivot.values])
+    y_train = np.array([y_train[i].nonzero()[0] for i in range(len(y_train))])
+    y_train = y_train.flatten()
+    print(y_train)
     x_train = X
 
     ytr = y_train[train_idx]
@@ -90,4 +108,5 @@ if __name__ == '__main__':
     partition = {'train': xtr, 'validation': xv}
     labels = {'train': ytr, 'validation': yv}
 
-    train_l2(partition,labels,val=True)
+    model = train_l2(partition,labels,val=False)
+    generate_predictions(models, preprocess, model)
